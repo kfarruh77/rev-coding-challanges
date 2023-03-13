3dba5cfdaecf6bd9ac0de1074f704641fd763234
kfarruh77 <kfarruh77@gmail.com>
2023-03-13 16:52:25 -0400

    addition function

---
+++ challenge.js
@@ -0,0 +1,9 @@
+// function to add two numbers
+function addition(num, num2) {
+  return num + num2;
+}
+
+// tests
+console.log(addition(3, 2));
+console.log(addition(-3, -6));
+console.log(addition(7, 3));
